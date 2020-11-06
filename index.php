237dedbc2d76a7af22852e20d7084d2bce392fd6
Allan Roque <allanrafaelroque@gmail.com>
2020-11-06 14:42:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Overpass:100,200,300,400&display=swap');
+
+        body {
+            color: white;
+            background-color: black;
+            text-align: center;
+        }
+        .header {
+            font-size: 50px;
+            margin-top: 350px;
+            font-family: 'Overpass';
+            font-weight: 100;
+        }
+        .logo {
+            width: 10%;
+            height: 10%;
+            margin-top: 225px;
+        }
+    </style>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>apache02</title>
+</head>
+
+<body>
+    <div class="header">
 <?php
-echo "<h1>Openshift Workshop v3.0</h1> ";
-echo "<h1>Openshift Workshop v3.0</h1> ";
-echo "<h1>Openshift Workshop v3.0</h1> ";
-echo "<h1>Openshift Workshop v3.0</h1> ";
+echo "<h1>Openshift Workshop v4</h1> ";
 echo $_SERVER['SERVER_ADDR']
 ?>
+    </div>
+</body>
+
+</html>
